cd748f15e5f42f130a37e3d12ab00432bac592c5
Krzysztof Nowakowski <krzychnowakowski@gmail.com>
2020-11-25 10:54:13 +0100

    Update scale.sh

--- scale.sh
+++ scale.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 
-docker service scale app_glowna_backend=5
-docker service scale app_glowna_frontend=5
-docker service scale app_glowna_celery=5
-docker service scale app_glowna_celery-beat=5
-docker service scale app_glowna_db=5
-docker service scale app_glowna_rabbitmq=5
-docker service scale app_test_backend-test=5
-docker service scale app_test_frontend-test=5
-docker service scale app_test_db-test=5
-docker service scale app_test_rabbitmq=5
-docker service scale app_test_celery=5
+docker service scale app_glowna_backend=$1
+#docker service scale app_glowna_frontend=$1
+docker service scale app_glowna_celery=$1
+#docker service scale app_glowna_celery-beat=$1
+#docker service scale app_glowna_db=$1
+#docker service scale app_glowna_rabbitmq=$1
+#docker service scale app_test_backend-test=$2
+#docker service scale app_test_frontend-test=$2
+#docker service scale app_test_db-test=$2
+#docker service scale app_test_rabbitmq=$2
+#docker service scale app_test_celery=$2
